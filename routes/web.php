4be3c71a4225a2e136ef99db49df44f2eece3a7d
sital00 <sitalmahato077@gmail.com>
2026-04-27 21:14:04 +0545

    Fix: Add GET redirect for result/submit route to prevent MethodNotAllowedHttpException

--- routes/web.php
+++ routes/web.php
@@ -63,6 +63,9 @@ Route::get('/question-bank', [HomeController::class, 'questionBank'])->name('pub
 Route::get('/gallery', [HomeController::class, 'gallery'])->name('public.gallery');
 Route::get('/result', [HomeController::class, 'result'])->middleware('throttle:result-check')->name('public.result');
 Route::post('/result/submit', [HomeController::class, 'resultSubmit'])->middleware('throttle:result-check')->name('public.result.submit');
+Route::get('/result/submit', function () {
+    return redirect()->route('public.result');
+})->name('public.result.submit.redirect');
 Route::get('/people', [HomeController::class, 'people'])->name('public.people');
 Route::get('/people/{type}/{id}', [HomeController::class, 'peopleProfile'])
     ->where('type', 'hod|teacher|staff')
